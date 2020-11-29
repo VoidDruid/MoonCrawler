9abe4d16befffe5c20ab235044fcd23bef00bdca
Kovalev Vasiliy <minskij10@yandex.ru>
2020-11-30 01:52:26 +0300

    f

--- src/Game/MainHeroIOSystem.cpp
+++ src/Game/MainHeroIOSystem.cpp
@@ -15,4 +15,22 @@ MoonCrawler::MainHeroIOSystem::operator()(std::shared_ptr<Scene> scene, std::sha
         pos.x--;
         std::cout <<"position: " << pos.x << " " << pos.y << std::endl;
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        auto& pos = components.positions[entity->ID];
+        pos.x++;
+        std::cout <<"position: " << pos.x << " " << pos.y << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        auto& pos = components.positions[entity->ID];
+        pos.y--;
+        std::cout <<"position: " << pos.x << " " << pos.y << std::endl;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        auto& pos = components.positions[entity->ID];
+        pos.y++;
+        std::cout <<"position: " << pos.x << " " << pos.y << std::endl;
+    }
 }
